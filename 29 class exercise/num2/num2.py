c0f99fdb570025261773c5a383dac902d3315fc2
Abigail <91783712+priscillabigaill@users.noreply.github.com>
2022-11-29 15:01:20 +0700

    Add files via upload

---
+++ 29 class exercise/num2/num2.py
@@ -0,0 +1,47 @@
+import math 
+
+class Circle:
+
+    def __init__(self, radius = 1.0, color = "red"):
+        self.__radius = radius
+        self.__color = color
+
+    def getRadius(self):
+        return self.__radius
+
+    def setRadius(self,radius):
+        self.__radius = radius
+
+    def getColor(self):
+        return self.__color
+
+    def setColor(self,color):
+        self.__color = color
+
+    def toString(self):
+        return f'Radius : {self.getRadius()}, Color: {self.getColor()}' 
+
+    def getArea(self):
+        return math.pi * pow(self.getRadius(),2)
+
+class Cylinder(Circle):
+
+    def __init__(self, radius, color, height = 1.0):
+        super().__init__(radius, color)
+        self.__height = height
+
+    def getHeight(self):
+        return self.__height
+
+    def setHeight(self,height):
+        self.__height = height
+
+    def toString(self):
+        return f'Radius : {self.getRadius()}, Color: {self.getColor()}, Height: {self.getHeight()}'
+
+    def getVolume(self):
+        return self.getArea() * self.getHeight()
+
+
+
+
---
+++ 29 class exercise/num2/run.py
@@ -0,0 +1,8 @@
+from num2 import *
+
+def main():
+    cyl = Cylinder(1.0,"red",1.0)
+    print(cyl.getVolume())
+
+if __name__ == "__main__":
+    main()
